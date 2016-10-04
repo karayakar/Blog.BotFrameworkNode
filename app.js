f2ebfb5e699b817202cb61f2433b27bfe6601005
Samuele Resca <samuele.resca@gmail.com>
2016-10-04 22:05:37 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
+
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: "66dd1dc9-3070-4337-896f-77e8af460f33",
+    appPassword: "ujQfy6CfhCnPwFJ5VDckZoX"
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+bot.dialog('/', function (session) {
+    session.send("Hello World");
+});
