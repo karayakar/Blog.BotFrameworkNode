8449ee20c566c066415c6f0b08fcfd3821db816c
Samuele Resca <samuele.resca@gmail.com>
2016-10-07 20:39:18 +0200

    fix configs

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var restify = require('restify');
 var builder = require('botbuilder');
-var configurationHelper = require('./Helpers/ConfigurationHelper.js');
+var config = require('./Helpers/ConfigurationHelper.js');
 
 //=========================================================
 // Bot Setup
@@ -13,7 +13,10 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 });
 
 // Create chat bot
-var connector = new builder.ChatConnector(configurationHelper.CONFIGURATIONS.CHAT_CONNECTOR);
+var connector = new builder.ChatConnector({
+    appId: config.CONFIGURATIONS.CHAT_CONNECTOR.APP_ID,
+    appPassword: config.CONFIGURATIONS.CHAT_CONNECTOR.APP_PASSWORD
+});
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
